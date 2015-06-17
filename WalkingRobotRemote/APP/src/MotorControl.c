836c991bf12fd1f863eb7430f29de2c05ce07110
myrh <cmm1991@gmail.com>
2015-06-17 13:54:33 +0200

    Fixed functions

--- WalkingRobotRemote/APP/src/MotorControl.c
+++ WalkingRobotRemote/APP/src/MotorControl.c
@@ -27,16 +27,16 @@
 #define RIGHT_BACK_BOT_MOTOR 			7
 
 #define LEFT_FRONT_TOP_MOTOR_INI		299
-#define LEFT_FRONT_BOT_MOTOR_INI		511 - 135
+#define LEFT_FRONT_BOT_MOTOR_INI		376
 #define	RIGHT_FRONT_TOP_MOTOR_INI		703
-#define RIGHT_FRONT_BOT_MOTOR_INI		511 + 135
+#define RIGHT_FRONT_BOT_MOTOR_INI		646
 #define	LEFT_BACK_TOP_MOTOR_INI			299
-#define LEFT_BACK_BOT_MOTOR_INI			511 + 135
+#define LEFT_BACK_BOT_MOTOR_INI			646
 #define	RIGHT_BACK_TOP_MOTOR_INI		703
-#define RIGHT_BACK_BOT_MOTOR_INI		511 - 135
+#define RIGHT_BACK_BOT_MOTOR_INI		376
 
-
-#define BOT_MOTOR_INI 					511
+#define RL_FRONT_BACK_BOT_MOTOR_INI 	646
+#define RL_BACK_FRONT_BOT_MOTOR_INI 	376
 #define LEFT_TOP_MOTOR_INI				299
 #define RIGHT_TOP_MOTOR_INI				703
 
@@ -44,6 +44,9 @@
 #define LOW_DELAY						75
 
 u16 old_speed = MAX_SPEED;
+
+void takeStep(u8 motor1, u8 motor2, u8 motor3, u8 motor4, u8 value, u8 side);
+void turnStep(u8 motor1, u8 motor2, u8 value);
 
 void init_motors()
 {
@@ -175,8 +178,9 @@ void turnLeftOnSpot(u16 speed)
 	mDelay(LOW_DELAY);
 
 	// take step
-	DXL_send_word(LEFT_BACK_BOT_MOTOR, GOAL_POSITION_L, LEFT_BACK_BOT_MOTOR_INI-135);
-	DXL_send_word(RIGHT_FRONT_BOT_MOTOR, GOAL_POSITION_L, RIGHT_FRONT_BOT_MOTOR_INI-135);
+	turnStep(RIGHT_FRONT_BOT_MOTOR, LEFT_BACK_BOT_MOTOR, -135, 1);
+	//DXL_send_word(LEFT_BACK_BOT_MOTOR, GOAL_POSITION_L, LEFT_BACK_BOT_MOTOR_INI-135);
+	//DXL_send_word(RIGHT_FRONT_BOT_MOTOR, GOAL_POSITION_L, RIGHT_FRONT_BOT_MOTOR_INI-135);
 
 //	// Set down legs
 //	setDownLegs(LEFT_BACK_TOP_MOTOR, RIGHT_FRONT_TOP_MOTOR);
@@ -211,8 +215,9 @@ void turnRightOnSpot(u16 speed)
 	mDelay(LOW_DELAY);
 
 	// take step
-	DXL_send_word(RIGHT_BACK_BOT_MOTOR, GOAL_POSITION_L, RIGHT_BACK_BOT_MOTOR_INI+135);
-	DXL_send_word(LEFT_FRONT_BOT_MOTOR, GOAL_POSITION_L, LEFT_FRONT_BOT_MOTOR_INI+135);
+	turnStep(RIGHT_BACK_BOT_MOTOR, LEFT_FRONT_BOT_MOTOR, 135, 0);
+	//DXL_send_word(RIGHT_BACK_BOT_MOTOR, GOAL_POSITION_L, RIGHT_BACK_BOT_MOTOR_INI+135);
+	//DXL_send_word(LEFT_FRONT_BOT_MOTOR, GOAL_POSITION_L, LEFT_FRONT_BOT_MOTOR_INI+135);
 
 
 	// Set down legs
@@ -233,31 +238,32 @@ void liftUpLegs(u8 motor1, u8 motor2, u8 value)
 
 void takeStep(u8 motor1, u8 motor2, u8 motor3, u8 motor4, u8 value, u8 side)
 {
-	int botMotorIni = getBotIni();
+	int botFrontBackMotorIni = getFrontBackBotIni();
+	int botBackFrontMotorIni = getBackFrontBotIni();
 	// Take a right or else take a left step
 
 	if(side == 1)
 	{
-		DXL_send_word(motor1, GOAL_POSITION_L, LEFT_BACK_BOT_MOTOR_INI - value);
-		DXL_send_word(motor2, GOAL_POSITION_L, RIGHT_FRONT_BOT_MOTOR_INI);
+		DXL_send_word(motor1, GOAL_POSITION_L, botFrontBackMotorIni - value);
+		DXL_send_word(motor2, GOAL_POSITION_L, botFrontBackMotorIni);
 
 		//DXL_send_word(motor1, GOAL_POSITION_L, botMotorIni);
 		//DXL_send_word(motor2, GOAL_POSITION_L, botMotorIni + value);
 
-		DXL_send_word(motor3, GOAL_POSITION_L, LEFT_FRONT_BOT_MOTOR_INI + value);
-		DXL_send_word(motor4, GOAL_POSITION_L, RIGHT_BACK_BOT_MOTOR_INI);
+		DXL_send_word(motor3, GOAL_POSITION_L, getBackFrontBotIni + value);
+		DXL_send_word(motor4, GOAL_POSITION_L, getBackFrontBotIni);
 
 		//DXL_send_word(motor3, GOAL_POSITION_L, botMotorIni);
 		//DXL_send_word(motor4, GOAL_POSITION_L, botMotorIni - value);
 	}
 	else
 	{
-		DXL_send_word(motor1, GOAL_POSITION_L, LEFT_BACK_BOT_MOTOR_INI );
-		DXL_send_word(motor2, GOAL_POSITION_L, RIGHT_FRONT_BOT_MOTOR_INI - value);
+		DXL_send_word(motor1, GOAL_POSITION_L, botFrontBackMotorIni );
+		DXL_send_word(motor2, GOAL_POSITION_L, botFrontBackMotorIni - value);
 
 
-		DXL_send_word(motor3, GOAL_POSITION_L, LEFT_FRONT_BOT_MOTOR_INI);
-		DXL_send_word(motor4, GOAL_POSITION_L, RIGHT_BACK_BOT_MOTOR_INI + value);
+		DXL_send_word(motor3, GOAL_POSITION_L, getBackFrontBotIni);
+		DXL_send_word(motor4, GOAL_POSITION_L, getBackFrontBotIni + value);
 
 
 //		DXL_send_word(motor1, GOAL_POSITION_L, botMotorIni + value);
@@ -269,9 +275,13 @@ void takeStep(u8 motor1, u8 motor2, u8 motor3, u8 motor4, u8 value, u8 side)
 
 }
 
-void turnStep(u8 motor1, u8 motor2, u8 value)
+void turnStep(u8 motor1, u8 motor2, u8 value, u8 side)
 {
-	int botMotorIni = getBotIni();
+	if(side == 1)
+		int botMotorIni = getFrontBackBotIni();
+	else
+		int botMotorIni = getBackFrontBotIni();
+	
 	DXL_send_word(motor1, GOAL_POSITION_L, botMotorIni + value);
 	DXL_send_word(motor2, GOAL_POSITION_L, botMotorIni + value);
 }
@@ -286,7 +296,12 @@ int getRightTopIni()
 	return RIGHT_TOP_MOTOR_INI;
 }
 
-int getBotIni()
+int getFrontBackBotIni()
 {
-	return BOT_MOTOR_INI;
+	return RL_FRONT_BACK_BOT_MOTOR_INI;
+}
+
+int getBackFrontBotIni()
+{
+	return RL_BACK_FRONT_BOT_MOTOR_INI;
 }
